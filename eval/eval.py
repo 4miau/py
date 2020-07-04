fc5eca64abc779d2dc5d7463b998910d0e08785b
miau <ghetto_star54@mail.com>
2020-07-04 23:41:19 +0100

    Update eval.py

--- eval/eval.py
+++ eval/eval.py
@@ -1,39 +1,79 @@
-import io
-import re
-import contextlib
+import ast
 import discord
+
 from discord.ext import commands
+
+class eval(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        print(message.content)
+
+        @commands.Cog.listener()
+        def insert_returns(body):
+    # insert return stmt if the last expression is a expression statement
+        if isinstance(body[-1], ast.Expr):
+        body[-1] = ast.Return(body[-1].value)
+        ast.fix_missing_locations(body[-1])
+
+    # for if statements, we insert returns into the body and the orelse
+    if isinstance(body[-1], ast.If):
+        insert_returns(body[-1].body)
+        insert_returns(body[-1].orelse)
+
+    # for with blocks, again we insert returns into the body
+    if isinstance(body[-1], ast.With):
+        insert_returns(body[-1].body)
+
+    # for with blocks, again we insert returns into the body
+    if isinstance(body[-1], ast.AsyncWith):
+        insert_returns(body[-1].body)
 
 @commands.command()
 @checks.has_permissions(PermissionLevel.OWNER)
-async def help(ctx):
-    embed = discord.Embed(title="Help", color=0x3975AA)
-    embed.add_field(name="help", value="Shows this message", inline=False)
-    embed.add_field(name="evaluate | eval | e", value="Evaluates the python code in the code block", inline=False)
-    return await ctx.send(embed=embed)
+async def eval_stmts(stmts, env=None):
+    """
+    Evaluates input.
+    If the last statement is an expression, that is the return value.
+    >>> from asyncio import run
+    >>> run(eval_stmts("1+1"))
+    2
+    >>> ctx = {}
+    >>> run(eval_stmts("ctx['foo'] = 1", {"ctx": ctx}))
+    >>> ctx['foo']
+    1
+    >>> run(eval_stmts('''
+    ... async def f():
+    ...    return 42
+    ...
+    ... await f()'''))
+    42
+    """
 
-@commands.command(aliases=["eval", "e"])
-@checks.has_permissions(PermissionLevel.OWNER)
-async def evaluate(ctx, *, command):
-    """Evaluate the given python code"""
-    if match := re.fullmatch(r"(?:\n*)?`(?:``(?:py(?:thon)?\n)?((?:.|\n)*)``|(.*))`", command, re.DOTALL):
-        code = match.group(1) if match.group(1) else match.group(2)
-        str_obj = io.StringIO()  # Retrieves a stream of data
-        try:
-            with contextlib.redirect_stdout(str_obj):
-                exec(code)
-        except Exception as e:
-            return await ctx.send(f"""❌ Your code completed with execution code 1
-```
-{e.__class__.__name__}: {e}
-```""")
-        return await ctx.send(f"""✅ Your code completed with execution code 0
-```
-{str_obj.getvalue()}
-```""")
-    embed = discord.Embed(description="Error: Invalid format", color=0xED2525)
-    return await ctx.send(embed=embed)
+    parsed_stmts = ast.parse(stmts)
+
+    fn_name = "_eval_expr"
+
+    fn = f"async def {fn_name}(): pass"
+    parsed_fn = ast.parse(fn)
+
+    for node in parsed_stmts.body:
+        ast.increment_lineno(node)
+
+    insert_returns(parsed_stmts.body)
+
+    parsed_fn.body[0].body = parsed_stmts.body
+    exec(compile(parsed_fn, filename="<ast>", mode="exec"), env)
+
+    return await eval(f"{fn_name}()", env)
 
 
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    
 def setup(bot):
     bot.add_cog(eval(bot))
